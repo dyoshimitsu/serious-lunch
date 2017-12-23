8f836c62f50724d098c0d34898905249a92f147d
dyoshimitsu <d.yoshimitsu@gmail.com>
2017-12-23 18:40:10 +0900

    fix deplpy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,14 +5,14 @@
 # config valid for current version and patch releases of Capistrano
 lock '~> 3.10.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'serious-lunch'
+set :repo_url, 'git@github.com:dyoshimitsu/serious-lunch.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/serious-lunch"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
