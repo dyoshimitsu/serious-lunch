28c3f09a26450b5d4f821b999718bce95e0384da
dyoshimitsu <d.yoshimitsu@gmail.com>
2017-12-24 19:44:06 +0900

    fix production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -44,7 +44,12 @@ server 'serious-lunch.com', user: 'ec2-user', roles: %w{app db web}
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
-#
+
+set :ssh_options,
+    keys: %w(~/.ssh/serious_lunch),
+    forward_agent: true,
+    auth_methods: %w(publickey)
+
 # The server-based syntax can be used to override options:
 # ------------------------------------
 # server "example.com",
