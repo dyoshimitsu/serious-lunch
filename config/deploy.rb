73613656453ce1b8aefed0070cb227bde6a14d2a
dyoshimitsu <d.yoshimitsu@gmail.com>
2017-12-29 00:21:12 +0900

    set value of capistrano3-puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,4 +49,16 @@ set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w[rake gem bundle ruby rails]
 
+# Set value of capistrano3-puma
+set :puma_threads, [4, 16]
+set :puma_workers, 0
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log, "#{release_path}/log/puma.error.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+
 # rubocop:enable Metrics/LineLength
