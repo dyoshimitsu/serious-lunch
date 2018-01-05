df64ffb39b31da353600a12f00c81815b9d7f120
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-01-05 19:13:27 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :v1 do
-    resource :hello, only: [:show]
+    resource :hello, only: [:show], controller: 'hello'
   end
 end
