57e6cffa3e0f5a17231845335e77a62eed0d70c5
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-01-10 00:00:29 +0900

    deploy test

--- scripts/travis_production_deploy.sh
+++ scripts/travis_production_deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]; then
-  ls -la ~/.ssh
   openssl aes-256-cbc -K $encrypted_3b954893797a_key -iv $encrypted_3b954893797a_iv -in config/serious_lunch.enc -out ~/.ssh/serious_lunch -d
-  chmod 600 ~/.ssh/serious_lunch
+  chmod 0700 ~/.ssh/serious_lunch
   bundle exec cap production deploy
 fi
