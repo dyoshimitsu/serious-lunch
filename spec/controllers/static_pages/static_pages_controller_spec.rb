1d5956befdb1b5c3bd5db37218fced2328ab50b3
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-01-15 00:52:02 +0900

    fix routes

---
+++ spec/controllers/static_pages/static_pages_controller_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, :type => :controller do
+
+  describe 'GET #home' do
+    before { get :home }
+
+    it { expect(response).to have_http_status(200) }
+  end
+
+  describe 'GET #about' do
+    before { get :about }
+
+    it { expect(response).to have_http_status(200) }
+  end
+
+  describe 'GET #signup' do
+    before { get :signup }
+
+    it { expect(response).to have_http_status(200) }
+  end
+
+  describe 'GET #hello' do
+    before { get :hello }
+
+    it { expect(response).to have_http_status(200) }
+    it { expect(response.body).to eq('hello, world!') }
+  end
+end
