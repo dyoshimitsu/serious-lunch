9e9b1950939264c451a35e9ac581b90ee018146e
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-01-16 20:36:00 +0900

    Add account model

---
+++ db/migrate/20180115110550_create_accounts.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateAccounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :accounts, :primary_key => :account_id do |t|
+      t.string :account_name, null: false, limit: 50
+      t.string :email, null: false
+
+      t.timestamps
+      t.index :account_name, unique: true
+      t.index :email, unique: true
+    end
+  end
+end
