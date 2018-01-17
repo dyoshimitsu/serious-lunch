41f368842150c57aca0dd92bf0b1aa07104fd9f6
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-01-18 00:19:44 +0900

    add spec

--- spec/controllers/accounts/accounts_controller_spec.rb
+++ spec/controllers/accounts/accounts_controller_spec.rb
@@ -9,4 +9,41 @@ RSpec.describe AccountsController, :type => :controller do
 
     it { expect(response).to have_http_status(200) }
   end
+
+  describe 'POST #create' do
+    before { post :create, params: params }
+
+    let(:account_name) { 'foo' }
+    let(:email) { 'foo@example.com' }
+    let(:password) { 'password' }
+    let(:password_confirmation) { 'password' }
+
+    let(:params) do
+      {
+        account:
+        {
+          account_name: account_name,
+          email: email,
+          password: password,
+          password_confirmation: password_confirmation,
+        },
+      }
+    end
+
+    context 'when parameter is valid' do
+      it 'create new account' do
+        expect(response).to have_http_status(204)
+        expect(Account.count).to eq(1)
+      end
+    end
+
+    context 'when password confirmation does not match' do
+      let(:password_confirmation) { 'password!' }
+
+      it 'not create new account' do
+        expect(response).to have_http_status(200)
+        expect(Account.count).to eq(0)
+      end
+    end
+  end
 end
