0bc38475009ac6015c8b38e66306f3b536368db2
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-01-17 20:04:26 +0900

    fix layout

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -2,9 +2,9 @@
 
 module AccountsHelper
 
-  def gravatar_for(account)
+  def gravatar_for(account, size: 80)
     gravatar_id = Digest::MD5.hexdigest(account.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: account.account_name, class: 'gravatar')
   end
 end
