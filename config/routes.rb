3ecf98199c4239598eddd46bf84c5b98530b4797
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-01-17 19:30:26 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :hello, only: [:show], controller: :static_pages, action: :hello
 
   root controller: :static_pages, action: :home
   resource :home, only: [:show], controller: :static_pages, action: :home
   resource :about, only: [:show], controller: :static_pages, action: :about
 
   resource :signup, only: [:show], controller: :accounts, action: :new
-
-  resource :hello, only: [:show], controller: :static_pages, action: :hello
+  resources :account, only: [:show], param: :account_name
+  get '/:account_name', controller: :accounts, action: :show
 end
