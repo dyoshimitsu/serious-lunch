17e4257688a4c2f8993135da157cd2c19b936618
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-01-17 20:55:54 +0900

    add create accout

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -9,4 +9,24 @@ class AccountsController < ApplicationController
   def show
     @account = Account.find_by(account_name: params[:account_name])
   end
+
+  def create
+    @account = Account.new(account_params)
+    if @account.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(
+      :account_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
 end
