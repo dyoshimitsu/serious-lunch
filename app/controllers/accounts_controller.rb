cf063948c14c35e25542820d9c19542e8937b971
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-01-17 20:28:52 +0900

    Add account form

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,6 +2,10 @@
 
 class AccountsController < ApplicationController
 
+  def new
+    @account = Account.new
+  end
+
   def show
     @account = Account.find_by(account_name: params[:account_name])
   end
