480fa97eedea07c8ae857bf9ca2602e48dbd6e9c
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-01-18 17:30:00 +0900

    fix validates

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 class Account < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   has_secure_password
 
+  before_save { email.downcase! }
   validates_with Validators::NonAccountNameValidator
-  validates :account_name, format: { with: /\A[0-9a-zA-Z_]+\z/ }
-  validates :password, length: { minimum: 8 }
+  validates :account_name, length: { maximum: 50 }, format: { with: /\A[0-9a-zA-Z_]+\z/ }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
