58d7b12ee2ce10a5b43dad3bcd47671337865092
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-01-19 17:44:34 +0900

    fix validates

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,9 +7,17 @@ class Account < ApplicationRecord
 
   before_save { email.downcase! }
   validates_with Validators::NonAccountNameValidator
-  validates :account_name, length: { maximum: 50 }, format: { with: /\A[0-9a-zA-Z_]+\z/ }
-  validates :password, presence: true, length: { minimum: 8 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  validates :account_name,
+            presence: true,
+            length: { maximum: 50 },
+            format: { with: /\A[0-9a-zA-Z_]+\z/ },
+            uniqueness: { case_sensitive: false }
+  validates :password,
+            presence: true,
+            length: { minimum: 8 }
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 end
