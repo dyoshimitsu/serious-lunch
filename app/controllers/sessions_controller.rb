b20cc338f5b3baadd2fa63bf86a4e74521b744fd
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-01-21 23:13:23 +0900

    add create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+
+  def create
+    account = Account.find_by(email: params[:session][:email].downcase)
+    if account&.authenticate(params[:session][:password])
+      log_in account
+      redirect_to account_url(@account.account_name)
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 end
