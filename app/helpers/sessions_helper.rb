bac50c3f72c2534552ac205dfda788a07b9fef1f
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-01-22 14:02:01 +0900

    add current_account

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,4 +5,8 @@ module SessionsHelper
   def log_in(account)
     session[:account_id] = account.account_id
   end
+
+  def current_account
+    @current_account ||= Account.find_by(account_id: session[:account_id])
+  end
 end
