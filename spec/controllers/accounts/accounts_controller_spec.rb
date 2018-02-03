a1704334dcd9e7bb655ed3cfdc3ba79542040efe
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-02-03 18:40:37 +0900

    add spec

--- spec/controllers/accounts/accounts_controller_spec.rb
+++ spec/controllers/accounts/accounts_controller_spec.rb
@@ -47,4 +47,37 @@ RSpec.describe AccountsController, :type => :controller do
       end
     end
   end
+
+  describe 'PATCH #update' do
+    before { patch :update, params: params }
+
+    let(:account) { FactoryBot.create :account }
+    let(:account_id) { account.account_id }
+
+    let(:account_name) { account.account_name }
+    let(:email) { account.email }
+    let(:password) { account.password }
+    let(:password_confirmation) { account.password }
+
+    let(:params) do
+      {
+        account_id: account_id,
+        account:
+        {
+          account_name: account_name,
+          email: email,
+          password: password,
+          password_confirmation: password_confirmation,
+        },
+      }
+    end
+
+    context 'when password confirmation does not match' do
+      let(:password_confirmation) { 'password!' }
+
+      it 'not update account' do
+      end
+    end
+  end
+
 end
