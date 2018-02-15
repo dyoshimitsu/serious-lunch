7bc9243552650fb8ba974cf656d130c99cf80241
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-02-16 08:51:14 +0900

    fix authenticated

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,7 +17,7 @@ module SessionsHelper
       @current_account ||= Account.find_by(account_id: account_id)
     elsif (account_id = cookies.signed[:account_id])
       account = Account.find_by(account_id: account_id)
-      if account&.authenticated?(cookies[:remember_token])
+      if account&.authenticated?(:remember, cookies[:remember_token])
         log_in account
         @current_account = account
       end
