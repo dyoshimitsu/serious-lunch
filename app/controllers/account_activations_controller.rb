62e8efd348d86f0b8ea30ca2989200d080d67b2a
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-02-16 08:57:38 +0900

    add account_activations_controller.rb

---
+++ app/controllers/account_activations_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class AccountActivationsController < ApplicationController
+
+  def edit
+    account = Account.find_by(email: params[:email])
+    if account && !account.activated? && account.authenticated?(:activation, params[:id])
+      acocunt_update_attribute(account)
+      log_in account
+      flash[:success] = 'Account activated!'
+      redirect_to short_account_url(account.account_name)
+    else
+      flash[:danger] = 'Invalid activation link'
+      redirect_to root_url
+    end
+  end
+
+  private
+
+  def acocunt_update_attribute(account)
+    account.update_attribute(:activated, true)
+    account.update_attribute(:activated_at, Time.zone.now)
+  end
+end
