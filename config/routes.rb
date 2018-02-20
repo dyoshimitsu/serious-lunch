bae8a65780180859645974e6806d625aa41279c2
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-02-20 09:38:39 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   delete '/logout', controller: :sessions, action: :destroy
 
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   get '/users', controller: :accounts, action: :index
   resources :accounts, only: [:update], param: :account_id
