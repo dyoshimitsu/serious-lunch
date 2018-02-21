639293fb881bbd995165ceeaae910ed434a9e59a
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-02-21 09:06:01 +0900

    refactor

--- app/models/account.rb
+++ app/models/account.rb
@@ -64,8 +64,7 @@ class Account < ApplicationRecord
 
   def create_reset_digest
     self.reset_token = Account.new_token
-    update_attribute(:reset_digest, Account.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: Account.digest(reset_token, reset_sent_at: Time.zone.now)
   end
 
   def send_password_reset_email
