ac3d8a15733e6d7dcadcf051e36b721bf851cf7c
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-02-22 20:40:05 +0900

    refactor password_resets_controller.rb

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,10 +5,10 @@ class PasswordResetsController < ApplicationController
   before_action :check_expiration, only: [:edit, :update]
 
   def create
-    @account = Account.find_by(email: params[:password_reset][:email].downcase)
-    if @account
-      @account.create_reset_digest
-      @account.send_password_reset_email
+    account = Account.find_by(email: params[:password_reset][:email].downcase)
+    if account
+      account.create_reset_digest
+      account.send_password_reset_email
       flash[:info] = 'Email sent with password reset instructions'
       redirect_to root_url
     else
