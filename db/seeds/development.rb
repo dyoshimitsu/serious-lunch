156b547e7f24785415440f64d8914810c1f0ac4c
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-03-04 11:35:19 +0900

    add seeds

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Account.find_or_create_by(account_id: 1) do |record|
+foo = Account.find_or_create_by(account_id: 1) do |record|
   record.assign_attributes(
     account_name: 'foo',
     email_address: 'foo@example.com',
@@ -17,5 +17,19 @@ Account.find_or_create_by(account_id: 2) do |record|
     password: 'password',
     activated: true,
     activated_at: Time.zone.now
+  )
+end
+
+VisitRestaurant.find_or_create_by(visit_restaurant_id: 1) do |record|
+  record.assign_attributes(
+    account: foo,
+    comment: 'excellent'
+  )
+end
+
+VisitRestaurant.find_or_create_by(visit_restaurant_id: 2) do |record|
+  record.assign_attributes(
+    account: foo,
+    comment: 'good'
   )
 end
