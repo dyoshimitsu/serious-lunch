76e6405a996204deb0e2526143008d70c2ae36ce
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-03-05 20:09:05 +0900

    fix default_scope

--- app/models/visit_restaurant.rb
+++ app/models/visit_restaurant.rb
@@ -2,7 +2,7 @@
 
 class VisitRestaurant < ActiveRecord::Base
   belongs_to :account
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(visit_date: :desc) }
 
   validates :account_id, presence: true
   validates :comment, length: { maximum: 255 }
