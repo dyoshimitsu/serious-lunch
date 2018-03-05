7286b4a071e705911449810d3a4404761b360c7a
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-03-05 20:08:23 +0900

    fix migration file

--- db/migrate/20180226135606_create_visit_restaurants.rb
+++ db/migrate/20180226135606_create_visit_restaurants.rb
@@ -5,11 +5,12 @@ class CreateVisitRestaurants < ActiveRecord::Migration[5.2]
     create_table :visit_restaurants, :primary_key => :visit_restaurant_id do |t|
       t.bigint :account_id, null: false
       t.string :comment
+      t.datetime :visit_date, null: false
 
       t.timestamps
     end
 
-    add_index :visit_restaurants, [:account_id, :created_at]
+    add_index :visit_restaurants, [:account_id, :visit_date]
     add_foreign_key :visit_restaurants,
                     :accounts,
                     primary_key: :account_id
--- db/schema.rb
+++ db/schema.rb
@@ -32,9 +32,10 @@ ActiveRecord::Schema.define(version: 2018_03_01_112447) do
   create_table "visit_restaurants", primary_key: "visit_restaurant_id", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.bigint "account_id", null: false
     t.string "comment"
+    t.datetime "visit_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["account_id", "created_at"], name: "index_visit_restaurants_on_account_id_and_created_at"
+    t.index ["account_id", "visit_date"], name: "index_visit_restaurants_on_account_id_and_visit_date"
   end
 
   add_foreign_key "visit_restaurants", "accounts", primary_key: "account_id"
