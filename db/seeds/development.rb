e94626f5d39a1a8a926a22851833800ef08232b6
Daiki Yoshimitsu <d.yoshimitsu@gmail.com>
2018-03-09 18:31:35 +0900

    Revert "User posts"

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-foo = Account.find_or_create_by(account_id: 1) do |record|
+Account.find_or_create_by(account_id: 1) do |record|
   record.assign_attributes(
     account_name: 'foo',
     email_address: 'foo@example.com',
@@ -10,36 +10,12 @@ foo = Account.find_or_create_by(account_id: 1) do |record|
   )
 end
 
-bar = Account.find_or_create_by(account_id: 2) do |record|
+Account.find_or_create_by(account_id: 2) do |record|
   record.assign_attributes(
     account_name: 'bar',
     email_address: 'bar@example.com',
     password: 'password',
     activated: true,
     activated_at: Time.zone.now
-  )
-end
-
-VisitRestaurant.find_or_create_by(visit_restaurant_id: 1) do |record|
-  record.assign_attributes(
-    account: foo,
-    visit_date: Time.zone.parse('2018-03-05T00:00+09:00'),
-    comment: 'excellent'
-  )
-end
-
-VisitRestaurant.find_or_create_by(visit_restaurant_id: 2) do |record|
-  record.assign_attributes(
-    account: bar,
-    visit_date: Time.zone.parse('2018-03-05T00:00+09:00'),
-    comment: 'bad'
-  )
-end
-
-VisitRestaurant.find_or_create_by(visit_restaurant_id: 3) do |record|
-  record.assign_attributes(
-    account: foo,
-    visit_date: Time.zone.parse('2018-03-04T00:00+09:00'),
-    comment: 'good'
   )
 end
