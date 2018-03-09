db7c12227c2784508866e763a2c7e6c1c628b9ac
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-03-09 20:25:31 +0900

    add 20180309112131_create_lunches.rb

---
+++ db/migrate/20180309112131_create_lunches.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class CreateLunches < ActiveRecord::Migration[5.2]
+  def change
+    create_table :lunches, :primary_key => :lunch_id do |t|
+      t.bigint :account_id, null: false
+      t.datetime :lunch_date, null: false
+      t.string :comment
+
+      t.timestamps
+    end
+
+    add_index :lunches, [:account_id, :lunch_date]
+    add_index :lunches, [:account_id, :created_at]
+    add_foreign_key :lunches,
+                    :accounts,
+                    primary_key: :account_id
+  end
+end
