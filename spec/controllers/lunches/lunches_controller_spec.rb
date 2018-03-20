ecb7fedbf2418659047cd7ca2408f3296d997b7b
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-03-20 22:17:08 +0900

    add spec

--- spec/controllers/lunches/lunches_controller_spec.rb
+++ spec/controllers/lunches/lunches_controller_spec.rb
@@ -61,7 +61,8 @@ RSpec.describe LunchesController, :type => :controller do
   describe 'DELETE #destroy' do
     let(:action) { delete :destroy, params: params }
 
-    let(:lunch) { FactoryBot.create :lunch }
+    let(:ate_lunch) { FactoryBot.create :account }
+    let(:lunch) { FactoryBot.create(:lunch, account: ate_lunch) }
     let(:params) do
       {
         lunch_id: lunch.lunch_id,
@@ -74,6 +75,36 @@ RSpec.describe LunchesController, :type => :controller do
       it 'should redirect' do
         expect(response).to have_http_status(302)
         expect(response).to redirect_to(login_url)
+      end
+    end
+
+    context 'when logged in' do
+      include SessionsHelper
+
+      before do
+        log_in(ate_lunch)
+        action
+      end
+
+      context 'when deleting the lunch of the logged in account' do
+        it 'deleted lunch' do
+          expect(response).to have_http_status(302)
+          expect(response).to redirect_to(root_url)
+          expect(flash[:success]).not_to be_nil
+          expect(ate_lunch.lunches.count).to eq(0)
+        end
+      end
+
+      context 'when deleting the lunch of the not logged in account' do
+        let(:not_ate_lunch) { FactoryBot.create :account }
+        let(:lunch) { FactoryBot.create(:lunch, account: not_ate_lunch) }
+
+        it 'not deleted lunch' do
+          expect(response).to have_http_status(302)
+          expect(response).to redirect_to(root_url)
+          expect(flash[:success]).to be_nil
+          expect(not_ate_lunch.lunches.count).to eq(1)
+        end
       end
     end
   end
