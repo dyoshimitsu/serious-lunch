800ceb469eb5a6bf3893e2dfcbe2f776aed5a790
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-03-25 00:00:34 +0900

    add migration query

--- db/migrate/20180324100535_create_account_resets.rb
+++ db/migrate/20180324100535_create_account_resets.rb
@@ -15,6 +15,23 @@ class CreateAccountResets < ActiveRecord::Migration[5.2]
                     :accounts,
                     primary_key: :account_id
 
+    sql = <<~SQL
+      INSERT INTO account_resets (
+        account_id,
+        reset_digest,
+        created_at,
+        updated_at
+      )
+        SELECT
+          account_id,
+          reset_digest,
+          reset_sent_at,
+          reset_sent_at
+        FROM accounts
+        WHERE reset_digest IS NOT NULL
+    SQL
+    ActiveRecord::Base.connection.execute(sql)
+
     remove_column :accounts, :reset_sent_at
     remove_column :accounts, :reset_digest
   end
@@ -22,6 +39,18 @@ class CreateAccountResets < ActiveRecord::Migration[5.2]
   def down
     add_column :accounts, :reset_digest, :string, after: :activated_at
     add_column :accounts, :reset_sent_at, :datetime, after: :reset_digest
+
+    sql = <<~SQL
+      UPDATE
+          accounts,
+          account_resets
+      SET
+        accounts.reset_digest  = account_resets.reset_digest,
+        accounts.reset_sent_at = account_resets.created_at
+      WHERE
+        accounts.account_id = account_resets.account_id
+    SQL
+    ActiveRecord::Base.connection.execute(sql)
 
     drop_table :account_resets
   end
