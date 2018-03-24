9320fbd2e6157b35b901590fac449e85fb750d41
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-03-24 22:45:37 +0900

    fix 20180324100535_create_account_remembers.rb

--- db/migrate/20180324100535_create_account_remembers.rb
+++ db/migrate/20180324100535_create_account_remembers.rb
@@ -3,18 +3,25 @@
 class CreateAccountRemembers < ActiveRecord::Migration[5.2]
   def up
     create_table :account_remembers, id: false do |t|
-      t.references :account, null: false
+      t.bigint :account_id, null: false
       t.string :remember_digest, null: false
 
       t.timestamps
+
+      t.index :account_id, unique: true
     end
 
     add_foreign_key :account_remembers,
                     :accounts,
                     primary_key: :account_id
+
+    remove_column :accounts, :remember_digest
   end
 
   def down
+    add_column :accounts, :remember_digest, :string,
+               after: :password_digest
+
     drop_table :account_remembers
   end
 end
