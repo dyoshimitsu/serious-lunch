e350e0d00fb53ea6567cf352cb38ad2a365e8c09
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-03-24 20:17:42 +0900

    fix 20180324100543_create_account_activations.rb

--- db/migrate/20180324100543_create_account_activations.rb
+++ db/migrate/20180324100543_create_account_activations.rb
@@ -1,8 +1,19 @@
 class CreateAccountActivations < ActiveRecord::Migration[5.2]
-  def change
-    create_table :account_activations do |t|
+  def up
+    create_table :account_activations, id: false do |t|
+      t.references :account, null: false
+      t.boolean :activated, null: false, default: false
+      t.string :reset_digest, null: false
 
       t.timestamps
     end
+
+    add_foreign_key :account_activations,
+                    :accounts,
+                    primary_key: :account_id
+  end
+
+  def down
+    drop_table :account_activations
   end
 end
