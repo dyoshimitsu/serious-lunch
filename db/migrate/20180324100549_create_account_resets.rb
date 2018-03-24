95534645c3965d828a44c53777487c9a59a4cb62
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-03-24 20:18:04 +0900

    20180324100549_create_account_resets.rb

--- db/migrate/20180324100549_create_account_resets.rb
+++ db/migrate/20180324100549_create_account_resets.rb
@@ -1,8 +1,18 @@
 class CreateAccountResets < ActiveRecord::Migration[5.2]
-  def change
-    create_table :account_resets do |t|
+  def up
+    create_table :account_resets, id: false do |t|
+      t.references :account, null: false
+      t.string :reset_digest, null: false
 
       t.timestamps
     end
+
+    add_foreign_key :account_resets,
+                    :accounts,
+                    primary_key: :account_id
+  end
+
+  def down
+    drop_table :account_resets
   end
 end
