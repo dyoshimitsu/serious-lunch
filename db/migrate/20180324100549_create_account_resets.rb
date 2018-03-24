69db8c70c4d41a22c5113c9fb4afe0aac1ddaa89
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-03-24 22:46:15 +0900

    fix 20180324100549_create_account_resets.rb

--- db/migrate/20180324100549_create_account_resets.rb
+++ db/migrate/20180324100549_create_account_resets.rb
@@ -3,18 +3,26 @@
 class CreateAccountResets < ActiveRecord::Migration[5.2]
   def up
     create_table :account_resets, id: false do |t|
-      t.references :account, null: false
+      t.bigint :account_id, null: false
       t.string :reset_digest, null: false
 
       t.timestamps
+
+      t.index :account_id, unique: true
     end
 
     add_foreign_key :account_resets,
                     :accounts,
                     primary_key: :account_id
+
+    remove_column :accounts, :reset_sent_at
+    remove_column :accounts, :reset_digest
   end
 
   def down
+    add_column :accounts, :reset_digest, :string, after: :activated_at
+    add_column :accounts, :reset_sent_at, :datetime, after: :reset_digest
+
     drop_table :account_resets
   end
 end
