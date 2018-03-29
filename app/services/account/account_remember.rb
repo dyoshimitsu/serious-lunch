db07b634333f396af26b30c3d9fd263924c68940
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-03-29 09:17:37 +0900

    add account_remember.rb

---
+++ app/services/account/account_remember.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Account::AccountRemember
+  include Virtus.model
+
+  attribute :account, Account, reader: :private
+
+  def account_remember
+    account.remember_token = Account::AccountAuthenticator.new_token
+    AccountCookie.create(
+      account_id: account.account_id,
+      reset_digest: Account::AccountAuthenticator.digest(account.remember_token)
+    )
+  end
+
+  def account_forget
+    AccountCookie.delete(account_id: @account)
+  end
+end
