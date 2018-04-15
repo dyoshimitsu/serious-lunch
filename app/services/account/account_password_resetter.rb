b2c90127722ca83424e55d4adc0d1ed4920d33be
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-04-16 00:26:30 +0900

    fix account_password_resetter.rb

--- app/services/account/account_password_resetter.rb
+++ app/services/account/account_password_resetter.rb
@@ -7,10 +7,24 @@ class Account::AccountPasswordResetter
 
   def account_password_reset
     account.reset_token = Account::AccountAuthenticator.new_token
-    AccountReset.create(
-      account_id: account.account_id,
-      reset_digest: Account::AccountAuthenticator.digest(account.reset_token)
-    )
+    digest = Account::AccountAuthenticator.digest(account.reset_token)
+    time = Time.zone.now.strftime('%F %T')
+
+    sql = <<~SQL
+      INSERT INTO account_resets (
+        account_id, reset_digest, created_at, updated_at
+      ) VALUES (
+        #{account.account_id},
+        '#{digest}',
+        '#{time}',
+        '#{time}'
+      )
+      ON DUPLICATE KEY UPDATE
+        reset_digest = '#{digest}',
+        updated_at   = '#{time}'
+    SQL
+    ActiveRecord::Base.connection.execute(sql)
+
     Account::AccountMailer.new(account: account).send_password_reset_email
   end
 
