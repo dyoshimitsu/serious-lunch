ba227b5c271de1e520f2f416e5be3e791d45acf8
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-04-20 08:39:27 +0900

    update account_remember.rb

--- app/services/account/account_remember.rb
+++ app/services/account/account_remember.rb
@@ -7,10 +7,23 @@ class Account::AccountRemember
 
   def account_remember
     account.remember_token = Account::AccountAuthenticator.new_token
-    AccountCookie.create(
-      account_id: account.account_id,
-      remember_digest: Account::AccountAuthenticator.digest(account.remember_token)
-    )
+    digest = Account::AccountAuthenticator.digest(account.remember_token)
+    time = Time.zone.now.strftime('%F %T')
+
+    sql = <<~SQL
+      INSERT INTO account_cookies (
+        account_id, remember_digest, created_at, updated_at
+      ) VALUES (
+        #{account.account_id},
+        '#{digest}',
+        '#{time}',
+        '#{time}'
+      )
+      ON DUPLICATE KEY UPDATE
+        remember_digest = '#{digest}',
+        updated_at      = '#{time}'
+    SQL
+    ActiveRecord::Base.connection.execute(sql)
   end
 
   def account_forget
