7d1cc88f16ce51384a395c164522831da57b37ef
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-04-25 08:50:51 +0900

    add seed

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -55,3 +55,10 @@ Lunch.find_or_create_by(lunch_id: 3) do |record|
     comment: 'good'
   )
 end
+
+AccountRelationship.find_or_create_by(follower_account_id: foo.account_id) do |record|
+  record.assign_attributes(
+    follower_account: foo,
+    followed_account: bar
+  )
+end
