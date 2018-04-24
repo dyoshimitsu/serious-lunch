71bcd896ff52c91ed16c29de31a4ce3a22df6d79
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-04-24 10:09:19 +0900

    add validates

--- app/models/account_relationship.rb
+++ app/models/account_relationship.rb
@@ -3,4 +3,7 @@
 class AccountRelationship < ApplicationRecord
   belongs_to :follower_account, class_name: 'Account'
   belongs_to :followed_account, class_name: 'Account'
+
+  validates :follower_account_id, presence: true
+  validates :followed_account_id, presence: true
 end
