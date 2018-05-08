d7d07c3165c83a3dbff0522320bd35444bcca9c6
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-05-09 01:13:40 +0900

    add create to account_relationships_controller.rb

--- app/controllers/account_relationships_controller.rb
+++ app/controllers/account_relationships_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class AccountRelationshipsController < ApplicationController
+  before_action :logged_in_account
+
+  def create
+    account = Account.find(params[:followed_account_id])
+    current_account.follow(account)
+    redirect_to account
+  end
 end
