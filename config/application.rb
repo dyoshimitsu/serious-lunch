6bbef281d6f466225f671a92103c0a17b4d9a23c
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-05-10 10:01:30 +0900

    add embed_authenticity_token_in_remote_forms

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,7 @@ module SeriousLunch
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
