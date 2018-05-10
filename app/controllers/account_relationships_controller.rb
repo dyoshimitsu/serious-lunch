f276e1f7ddbb99d1fef749fd7347f2f27e6f8450
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-05-10 10:01:12 +0900

    update account_relationships_controller.rb

--- app/controllers/account_relationships_controller.rb
+++ app/controllers/account_relationships_controller.rb
@@ -6,12 +6,18 @@ class AccountRelationshipsController < ApplicationController
   def create
     account = Account.find(params[:followed_account_id])
     Account::AccountFollower.new(account: current_account).follow(account)
-    redirect_to(short_account_url(account.account_name))
+    respond_to do |format|
+      format.html { redirect_to(short_account_url(account.account_name)) }
+      format.js
+    end
   end
 
   def destroy
     account = AccountRelationship.find(params[:account_relationship_id]).followed_account
     Account::AccountFollower.new(account: current_account).unfollow(account)
-    redirect_to(short_account_url(account.account_name))
+    respond_to do |format|
+      format.html { redirect_to(short_account_url(account.account_name)) }
+      format.js
+    end
   end
 end
