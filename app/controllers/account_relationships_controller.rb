3930a284e3dc9fbf3c3c69ddf034e6c9937c010c
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-05-11 20:13:23 +0900

    fix bug

--- app/controllers/account_relationships_controller.rb
+++ app/controllers/account_relationships_controller.rb
@@ -4,19 +4,19 @@ class AccountRelationshipsController < ApplicationController
   before_action :logged_in_account
 
   def create
-    account = Account.find(params[:followed_account_id])
-    Account::AccountFollower.new(account: current_account).follow(account)
+    @account = Account.find(params[:followed_account_id])
+    Account::AccountFollower.new(account: current_account).follow(@account)
     respond_to do |format|
-      format.html { redirect_to(short_account_url(account.account_name)) }
+      format.html { redirect_to(short_account_url(@account.account_name)) }
       format.js
     end
   end
 
   def destroy
-    account = AccountRelationship.find(params[:account_relationship_id]).followed_account
-    Account::AccountFollower.new(account: current_account).unfollow(account)
+    @account = AccountRelationship.find(params[:account_relationship_id]).followed_account
+    Account::AccountFollower.new(account: current_account).unfollow(@account)
     respond_to do |format|
-      format.html { redirect_to(short_account_url(account.account_name)) }
+      format.html { redirect_to(short_account_url(@account.account_name)) }
       format.js
     end
   end
