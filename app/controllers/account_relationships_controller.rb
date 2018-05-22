f252dc911fce7656b2b64fa043b3d48f9ec5a32e
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-05-23 00:29:30 +0900

    fix account_relationships_controller.rb

--- app/controllers/account_relationships_controller.rb
+++ app/controllers/account_relationships_controller.rb
@@ -4,9 +4,16 @@ class AccountRelationshipsController < ApplicationController
   before_action :logged_in_account
 
   def create
-    account = Account.find(params[:followed_account_id])
-    Account::AccountFollower.new(account: current_account).follow(account)
-    redirect_to(short_account_url(account.account_name))
+    account = Account.find_by(account_id: params[:followed_account_id])
+    if account && account != current_account
+      Account::AccountFollower.new(account: current_account).follow(account)
+      redirect_to(short_account_url(account.account_name))
+    else
+      render file: Rails.root.join('public/404.html'),
+             status: 404,
+             layout: false,
+             content_type: 'text/html'
+    end
   end
 
   def destroy
